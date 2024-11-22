113712a12d503d325afafc21d95c50eb128b93e1
PocheonLim <dlatjdgns010@naver.com>
2024-11-22 18:11:58 +0900

    전화번호 입력 예외처리, 중복처리

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '1234',
-    database: 'precapstonedb'
+    database: 'pstonedbpreca'
 });
 // db연결
 db.connect(err => {
     if (err) {
-        //console.error('DB 연결 실패:', err);
+        console.error('DB 연결 실패:', err);
         return;
     }
     console.log('MySQL 연결 성공!');
